d2b006956ff40e19fa5ac8af4f0da92376dcb63d
Jethan-B4 <barcenasbenedict@gmail.com>
2026-09-09 21:02:02 +0800

    Streaming (Lazy Loading), Suspense, Route Groups

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,7 +15,13 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+  // const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+    const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
