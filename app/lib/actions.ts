288b43f7296649820b31b7eda01bc0ef3b9646e7
Jethan-B4 <barcenasbenedict@gmail.com>
2026-09-21 15:40:09 +0800

    Auth configuration fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,7 +31,11 @@ export async function authenticate(
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);
+    const redirectTo = formData.get('redirectTo') as string | null;
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirectTo: redirectTo || '/dashboard',
+    });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
